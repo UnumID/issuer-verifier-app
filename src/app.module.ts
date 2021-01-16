61032bab710e2326854766da2bc2a7eb104a3d53
Ray Smets <rayjsmets@gmail.com>
2021-01-15 17:56:24 -0800

    [Credential] service, controller, and module generated.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CredentialsController } from './credentials/credentials.controller';
+import { CredentialsService } from './credentials/credentials.service';
+import { CredentialsModule } from './credentials/credentials.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CredentialsModule],
 })
 export class AppModule {}
