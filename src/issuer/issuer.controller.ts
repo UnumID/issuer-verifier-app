f9968fdf33935c4a71c14633c5de2ecf403713c5
Ray Smets <rayjsmets@gmail.com>
2021-01-20 18:34:04 -0800

    wip.

--- src/issuer/issuer.controller.ts
+++ src/issuer/issuer.controller.ts
@@ -1,14 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Header, Post } from '@nestjs/common';
 import { IssuerService } from './issuer.service';
-
+import {
+  issueCredential as _issueCredential,
+  revokeCredential as _revokeCredential,
+  registerIssuer as _registerIssuer,
+  IssuerDto,
+  RegisteredIssuer
+} from '@UnumId/issuer-server-sdk';
 @Controller('issuer')
 export class IssuerController {
   constructor (private issuerService: IssuerService) {}
 
   // todo and real types to these requests
   @Post('register')
+  @Header('x-auth-token')
   async register (@Body() dto: any) {
-    return await this.issuerService.registerIssuer(dto.name, dto.customerUuid, dto.apiKey);
+    const res: IssuerDto<RegisteredIssuer> = await this.issuerService.registerIssuer(dto.name, dto.customerUuid, dto.apiKey);
+    const authToken = res.authToken;
   }
 
   @Post('issueCredential')
