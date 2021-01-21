10d3e79849491ef689c97d0aec535a4bad6f2433
Ray Smets <rayjsmets@gmail.com>
2021-01-21 07:37:03 -0800

    [Logs] config info printed at startup.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap () {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: true // todo place holder to have the logger actually use the log level defined in configurations
+  });
 
   const configService = app.get(ConfigService);
   const port = configService.get('port');
 
   await app.listen(port);
+
+  Logger.debug(`Backend app started on port: ${port}`);
+  Logger.debug(`Backend app started with log level: ${configService.get('logLevel')}`);
+  Logger.debug(`Backend app pointed at UnumID SaaS url: ${configService.get('saasUrl')}`);
 }
 bootstrap();
