cd4962330fedca527aea049ec36d420ec881c7d0
Ray Smets <rayjsmets@gmail.com>
2021-01-21 13:12:09 -0800

    [Test].

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,8 @@ async function bootstrap () {
 
   await app.listen(port);
 
-  Logger.debug(`Backend app started on port: ${port}`);
-  Logger.debug(`Backend app started with log level: ${configService.get('logLevel')}`);
+  Logger.debug(`TEST Backend app started on port: ${port}`);
+  Logger.debug(`TEST Backend app started with log level: ${configService.get('logLevel')}`);
   Logger.debug(`Backend app pointed at UnumID SaaS url: ${configService.get('saasUrl')}`);
 }
 bootstrap();
