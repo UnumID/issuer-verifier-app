0b31cc8b5f3de6f5e3358f7d128c786f2d385fd2
Ray Smets <rayjsmets@gmail.com>
2021-01-22 16:32:27 -0800

    [SDKs] versions updated to support new Proof type attribute. Local config env added.

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,7 +2,7 @@ import { Configuration } from '../interfaces/configuration.interface';
 
 export default (): Configuration =>
   ({
-    // Default
+    // Default Sandbox
     [process.env.NODE_ENV]: {
       saasUrl: process.env.SAAS_URL || 'https://api.sandbox-unumid.org/',
       logLevel: process.env.LOG_LEVEL || 'debug',
@@ -16,6 +16,11 @@ export default (): Configuration =>
     production: {
       saasUrl: process.env.SAAS_URL || 'https://api.unumid.org/',
       logLevel: process.env.LOG_LEVEL || 'info',
+      port: parseInt(process.env.PORT) || 4100
+    },
+    local: {
+      saasUrl: process.env.SAAS_URL || 'http://localhost:3030/',
+      logLevel: process.env.LOG_LEVEL || 'debug',
       port: parseInt(process.env.PORT) || 4100
     }
   }[process.env.NODE_ENV]);
