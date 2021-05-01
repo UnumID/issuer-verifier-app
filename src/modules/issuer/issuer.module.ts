072c9427509c8e77a309cec658c596df0e0e515f
Ray Smets <rayjsmets@gmail.com>
2021-04-30 22:29:48 -0700

    [Modules] updated.

--- src/modules/issuer/issuer.module.ts
+++ src/modules/issuer/issuer.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { IssuerV2Service } from 'src/issuer-v2/issuer-v2.service';
 import { IssuerController } from './issuer.controller';
 import { IssuerService } from './issuer.service';
 
 @Module({
   controllers: [IssuerController],
-  providers: [IssuerService]
+  providers: [IssuerService, IssuerV2Service]
 })
 export class IssuerModule {}
