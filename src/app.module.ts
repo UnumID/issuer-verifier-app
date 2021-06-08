51b89e97e93b5d39291e5ca6b72808b694bd218f
Ray Smets <rayjsmets@gmail.com>
2021-06-08 10:05:34 -0700

    [Verifer] v3 service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { IssuerModule } from './modules/issuer/issuer.module';
 import { VerifierModule } from './modules/verifier/verifier.module';
 import { VerifierV2Service } from './verifier-v2/verifier-v2.service';
 import { IssuerV2Service } from './issuer-v2/issuer-v2.service';
+import { VerifierV3Service } from './verifier-v3/verifier-v3.service';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { IssuerV2Service } from './issuer-v2/issuer-v2.service';
     IssuerModule,
     VerifierModule,
     HealthModule],
-  providers: [AuthGuard, VersionGuard, VerifierV2Service, IssuerV2Service]
+  // providers: [AuthGuard, VersionGuard, VerifierV2Service, IssuerV2Service, VerifierV3Service]
+  providers: [AuthGuard, VersionGuard]
 })
 export class AppModule {}
---
+++ src/verifier-v3/verifier-v3.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  registerVerifier as _registerVerifier,
+  sendEmail as _sendEmail,
+  sendSms as _sendSms,
+  sendRequest as _sendRequest,
+  verifyPresentation as _verifyPresentation,
+  RegisteredVerifier,
+  UnumDto,
+  DecryptedPresentation
+} from '@unumid/server-sdk';
+import { EncryptedData, PresentationRequestDto, PresentationRequestPostDto } from '@unumid/types';
+
+@Injectable()
+export class VerifierV3Service {
+  registerVerifier (name: string, customerUuid: string, url: string, apiKey: string): Promise<UnumDto<RegisteredVerifier>> {
+    try {
+      return _registerVerifier(name, customerUuid, url, apiKey);
+    } catch (error) {
+      Logger.error('Error handling registerVerifier with UnumID SaaS', error);
+      throw error;
+    }
+  }
+
+  sendEmail (authorization: string, to: string, deeplink: string): Promise<UnumDto> {
+    try {
+      return _sendEmail(authorization, to, deeplink);
+    } catch (error) {
+      Logger.error('Error handling sendEmail to UnumID SaaS', error);
+      throw error;
+    }
+  }
+
+  sendSms (authorization: string, to: string, deeplink: string): Promise<UnumDto> {
+    try {
+      return _sendSms(authorization, to, deeplink);
+    } catch (error) {
+      Logger.error('Error handling sendSms to UnumID SaaS', error);
+      throw error;
+    }
+  }
+
+  sendRequest (authorization:string, verifier: string, credentialRequests: [], eccPrivateKey: string, holderAppUuid: string, expirationDate?: Date, metadata?: Record<string, unknown>): Promise<UnumDto<PresentationRequestPostDto>> {
+    try {
+      return _sendRequest(authorization, verifier, credentialRequests, eccPrivateKey, holderAppUuid, expirationDate, metadata);
+    } catch (error) {
+      Logger.error('Error handling sendRequest to UnumID SaaS', error);
+      throw error;
+    }
+  }
+
+  async verifyPresentation (authorization: string, presentation: EncryptedData, verifier: string, encryptionPrivateKey: string, presentationRequest?: PresentationRequestDto): Promise<UnumDto<DecryptedPresentation>> {
+    try {
+      return await _verifyPresentation(authorization, presentation, verifier, encryptionPrivateKey, presentationRequest);
+    } catch (error) {
+      Logger.error('Error handling verifying encrypted presentation request to UnumID Saas.', error);
+      throw error;
+    }
+  }
+}
