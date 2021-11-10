db7aff71e08064109ba81bd42938d9ae0f1f83ed
Ray Smets <rayjsmets@gmail.com>
2021-11-10 14:44:44 -0800

    [SDK] version updated to latest which requires new env vars.

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,21 +6,25 @@ export default (): Configuration =>
     [process.env.NODE_ENV]: {
       saasUrl: process.env.SAAS_URL || 'https://api.sandbox-unumid.org/',
       logLevel: process.env.LOG_LEVEL || 'debug',
+      debug: true,
       port: parseInt(process.env.PORT) || 4100
     },
     development: {
       saasUrl: process.env.SAAS_URL || 'https://api.dev-unumid.org/',
       logLevel: process.env.LOG_LEVEL || 'debug',
+      debug: true,
       port: parseInt(process.env.PORT) || 4100
     },
     production: {
       saasUrl: process.env.SAAS_URL || 'https://api.unumid.org/',
       logLevel: process.env.LOG_LEVEL || 'info',
+      debug: false,
       port: parseInt(process.env.PORT) || 4100
     },
     local: {
       saasUrl: process.env.SAAS_URL || 'http://localhost:3030/',
       logLevel: process.env.LOG_LEVEL || 'debug',
+      debug: true,
       port: parseInt(process.env.PORT) || 4100
     }
   }[process.env.NODE_ENV]);
--- src/interfaces/configuration.interface.ts
+++ src/interfaces/configuration.interface.ts
@@ -1,5 +1,6 @@
 export interface Configuration {
     saasUrl: string,
     logLevel: string,
+    debug: boolean,
     port: number
 }
